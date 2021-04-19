ed463f513217d52e1c012bcad662ecc4b668543e
Sofia Darke <sofia.darke@hyperisland.se>
2021-04-19 21:24:24 +0200

    about text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,26 +10,29 @@ const About = () => {
 					My name is Sofia, I'm a curious frontend developer with a background
 					as a visual designer. Right now I'm on the lookout for an internship
 					far the fall 2021. React is one of my favourite framworks so far. The
-					focus in my studies are mainly front end but I can see myself going
+					focus in my studies are mainly frontend but I can see myself going
 					into a more fullstack position eventually. Coding is amazing.
 				</p>
 				<br />
 				<br />
 				<p className="tech-text">
-					<u>TECH-STACK</u> <br />
+					<u>TECH-STACK</u>
+					<br />
 					React
-					<br /> SCASS
-					<br /> Express
+					<br /> Express.js
 					<br /> Node.js
 					<br /> Gatsby
 					<br /> Figma
 					<br /> Git
 					<br /> Github
-					<br /> MySQL <br />
+					<br /> SASS
+					<br /> MySQL
+					<br /> Netlify
 					<br />
 				</p>
 				<p className="tech-text">
-					<u>TECH-learning</u> <br />
+					<u>And learning...</u>
+					<br />
 					Typescript
 					<br /> Tailwind
 					<br /> Vue
